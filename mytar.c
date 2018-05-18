3bd81f0ee79d8eb0ea5f510a5fcb937786a06ea2
Anthony <anthonypalazzo3@gmail.com>
2018-05-18 16:48:23 -0700

    Added mytar.c mytar.h

---
+++ mytar.c
@@ -0,0 +1,64 @@
+#include "mytar.h"
+#include <string.h>
+
+/* Helper function to get the given file from a string */
+FILE* get_fp(char path[256]) {
+   FILE *fp = fopen(path, "r");
+   if (fp == NULL) {
+      fprintf(stderr, "usage");
+      exit(EXIT_FAILURE);
+   }
+   
+   return fp;
+}
+
+/* Helper function to seperate the prefix and the name for a header if too long */
+int sep_prefix_name(char path[256]) {
+   int i;
+   char c;
+   for (i = 0; path[i]; i++) {
+      c = path[i];
+      if (((strlen(path) - i) <= 100) && c == '/') {
+         return i;
+      }
+   }
+   fprintf(stderr, "%s: unable to properly divide name. Skipping.\n", path);
+   exit(EXIT_FAILURE);
+}
+
+/* Returns a pointer to a complete posix_header from tar.h */
+struct Header* get_header(FILE* fp, char path[256]) {
+   Header *header;
+   struct stat *stat;
+   int i;
+   /* name[100], offset: 0; prefix[155], offset: 345 */
+   if (strlen(path) > 100) {
+      i = sep_prefix_name(path);
+      strncpy(header->name, (path + i + 1), (strlen(path) - i + 1));
+      strncpy(header->prefix, path, i - 1);
+   }
+   else {
+      strncpy(header->name, path, strlen(path));
+   }
+   /* Need lstat for the remaining fields */
+   if (lstat(path, stat) < 0) {
+      fprintf(stderr, "lstat failed\n");
+      exit(EXIT_FAILURE);
+   }
+   /* mode[8]; offset: 100 */
+   strmode(stat->st_mode, header->mode);
+   /* uid[8]; offset: 108 */
+   /* gid[8]; offset: 116 */
+   /* size[12]; offset: 124 */
+   /* mtime[12]; offset: 136 */
+   /* chksum[8]; offset: 148 */
+   /* typeflag; offset: 156 */
+   /* linkname[100]; offset: 157 */
+   /* magic[6]; offset: 257 */
+   /* version[2]; offset: 263 */
+   /* uname[32]; offset: 265 */
+   /* gname[32]; offset: 297 */
+   /* devmajor[8]; offset: 329 */
+   /* devminor[8]; offset: 337 */
+   return NULL;
+}
---
+++ mytar.h
@@ -0,0 +1,37 @@
+#ifndef MYTAR_H
+#define MYTAR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <tar.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/* Copy of the posix_header struct found in spec */
+typedef struct {
+  char name[100];
+  char mode[8];
+  char uid[8];
+  char gid[8];
+  char size[12];
+  char mtime[12];
+  char chksum[8];
+  char typeflag;
+  char linkname[100];
+  char magic[6];
+  char version[2];
+  char uname[32];
+  char gname[32];
+  char devmajor[8];
+  char devminor[8];
+  char prefix[155];
+} Header;
+
+FILE* get_fp(char path[256]);
+int sep_prefix_name(char path[256]);
+struct Header* get_header(FILE* fp, char path[256]);
+
+#endif
